4d924134d63d78e3d623489a3a9b67249a4a78a2
JassHsu <asmallboy.tw@gmail.com>
2017-06-19 18:30:09 +0800

    Add files via upload

--- FinalProject/finalproject.R
+++ FinalProject/finalproject.R
@@ -2,34 +2,30 @@ library(quantmod)
 
 
 # load APPLE data
-getSymbols("AAPL")
+stock<-get(getSymbols("IBM"))
 
-
-
-
+# set the date
+date<-"2012-03-30/2013-12-31"
 
 startpoints<- function(x,on="days",k=1){
   head(endpoints(x,on,k)+1,-1)
 }
 
-
+# create the profit function
 profit<-function(per,sigma){
 
-
-bb<-BBands(Cl(AAPL),n=per,sd=sigma)
-
+bb<-BBands(Cl(stock),n=per,sd=sigma)
 
 bb.up<-bb$up
 bb.dn<-bb$dn
 
-m<-merge(Cl(AAPL),BBands(Cl(AAPL),maType="SMA"))
-m$sig[with(m,AAPL.Close>up)&index(m) %in% index(m)[startpoints(m,on="days")]]<-0
-m$sig[with(m,AAPL.Close<up)&index(m) %in% index(m)[startpoints(m,on="days")]]<-1
-#m$sig[1:19]<-1
+m<-merge(Cl(stock),BBands(Cl(stock),maType="SMA"))
+m$sig[with(m,stock[,4]>bb.up)&index(m) %in% index(m)[startpoints(m,on="days")]]<-1
+m$sig[with(m,stock[,4]<bb.dn)&index(m) %in% index(m)[startpoints(m,on="days")]]<-0
 position<-na.locf(m)
 
-return<-ROC(Cl(AAPL))*position$sig
-return<-return["2012-03-30/2013-12-31"]
+return<-ROC(Cl(stock))*position$sig
+return<-return[date]
 return<-exp(cumsum(return))
 
 
@@ -37,4 +33,3 @@ return(return)
 }
 
 
-plot(return)
